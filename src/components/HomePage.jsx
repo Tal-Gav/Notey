366c3b5c4454384f66aeb7d233022d669bdc66e5
TalGav23 <talgav23@gmail.com>
2023-10-04 12:59:19 +0300

    fixed positioning with mui + "gap" css

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,7 @@
 import "./HomePage.css";
 import { Tilt } from "react-tilt";
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
 
 const defaultOptions = {
   reverse: false, // reverse the tilt direction
@@ -18,22 +14,11 @@ const defaultOptions = {
   reset: true, // If the tilt effect has to be reset on exit.
   easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
 };
+
 const HomePage = () => {
   const whiteBackground = "./images/white.jpg";
   const noteyLogo = "./images/notey.png";
 
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <Button>Item</Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Button>Item</Button>
-        </Grid>
-      </React.Fragment>
-    );
-  }
   return (
     <div>
       <div className="gradient-background">
@@ -43,8 +28,14 @@ const HomePage = () => {
         ></div>
 
         <div className="container">
-          <Grid container direction={"column"} spacing={4}>
-            <Grid item xl={6} md={6} sm={12} xs={12}>
+          <Grid
+            container
+            spacing={5}
+            direction="column"
+            alignItems="center"
+            minHeight="100vh"
+          >
+            <Grid item xl={6} md={6} sm={12} xs={12} sx={{ marginTop: 25 }}>
               <img className="notey-logo" src={noteyLogo} alt="Logo" />
             </Grid>
             <Grid item xl={6} md={6} sm={12} xs={12}>
@@ -52,20 +43,29 @@ const HomePage = () => {
                 Write down anything. easily.
               </div>
             </Grid>
-            <Grid item xl={6} md={6} sm={12} xs={12}>
-              <div className="buttons-container">
-                <Tilt options={defaultOptions}>
-                  <div className="sign-up-btn font-tt-fors-regular">
-                    <div className="btn-text">Sign Up</div>
-                  </div>
-                </Tilt>
-
-                <Tilt options={defaultOptions}>
-                  <div className="log-in-btn font-tt-fors-regular">
-                    <div className="btn-text">Log In</div>
-                  </div>
-                </Tilt>
-              </div>
+            <Grid
+              item
+              xl={6}
+              md={6}
+              sm={12}
+              xs={12}
+              sx={{
+                marginTop: 25,
+                display: "flex",
+                justifyContent: "center",
+                gap: "18vh",
+              }}
+            >
+              <Tilt options={defaultOptions}>
+                <div className="sign-up-btn font-tt-fors-regular">
+                  <div className="btn-text">Sign Up</div>
+                </div>
+              </Tilt>
+              <Tilt options={defaultOptions}>
+                <div className="log-in-btn font-tt-fors-regular">
+                  <div className="btn-text">Log In</div>
+                </div>
+              </Tilt>
             </Grid>
           </Grid>
         </div>
