89fd34ece1f5f644b2cc56fd4c836a93b905c274
TalGav23 <talgav23@gmail.com>
2023-10-06 15:09:18 +0300

    implementing NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <NavBar />
+
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
