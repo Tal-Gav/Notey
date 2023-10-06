6ff7d1bceb48571b19034b8c39c328633f05be01
TalGav23 <talgav23@gmail.com>
2023-10-07 02:24:58 +0300

    made base routes for future pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/Home/Home";
 import SignUp from "./components/SignUp/SignUp";
 import LogIn from "./components/LogIn/LogIn";
 import NavBar from "./components/NavBar/NavBar";
+import MyNotes from "./components/MyNotes/MyNotes";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/my notes" element={<MyNotes />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
