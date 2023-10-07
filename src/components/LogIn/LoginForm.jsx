64455f3d5b7366d58d52a6e178ad40b18603a60d
tal <talgav23@gmail.com>
2023-10-07 12:36:40 +0300

    started signup strcture

---
+++ src/components/LogIn/LoginForm.jsx
@@ -0,0 +1,8 @@
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div>login </div>;
+};
+
+export default LoginForm;
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,44 @@
+import React, { useState } from "react";
+
 const SignUpPage = () => {
-  return <div>signup</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <div>
+      <h1>signup</h1>
+      <div>
+        <h2>username</h2>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <h2>passwrod</h2>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>{" "}
+      <div>
+        <h2>confirmPassword</h2>
+        <input
+          type="text"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <div>username: {username}</div>
+        <div>password: {password}</div>
+        <div>confirmPassword: {confirmPassword}</div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
