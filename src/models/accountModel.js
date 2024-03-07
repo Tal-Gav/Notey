00cf014779e8406b638ee71fcce89ab5cc513d7a
Tal Gav <talgav23@gmail.com>
2024-03-07 21:40:53 +0200

    Added hash methods for the user's password

--- src/models/accountModel.js
+++ src/models/accountModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const accountSchema = mongoose.Schema(
   {
@@ -23,5 +24,25 @@ const accountSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+accountSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+accountSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (user) {
+    const passwordAuth = bcrypt.compare(password, user.password);
+
+    if (passwordAuth) {
+      throw Error("Incorrect password.");
+    }
+  } else {
+    throw Error("Account does not exist.");
+  }
+};
 // Create accounts collection
 export const Account = mongoose.model("Account", accountSchema);
